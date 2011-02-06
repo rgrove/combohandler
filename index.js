eb6c4c1d7b923014a449b95d6eae3c8d26481e9a
Ryan Grove <ryan@wonko.com>
2011-02-05 19:17:25 -0800

    Less binding, more nesting.

--- index.js
+++ index.js
@@ -43,39 +43,6 @@ exports.combine = function (config) {
       next();
     }
 
-    function onReadFile(i, err, data) {
-      if (err) { return next(new BadRequest); }
-
-      body[i]  = data;
-      pending -= 1;
-
-      if (pending === 0) {
-        finish();
-      }
-    }
-
-    function onRealPath(i, err, absolutePath) {
-      // Bubble up an error if the file can't be found or if the request
-      // attempts to traverse above the root path.
-      if (err || !absolutePath || absolutePath.indexOf(rootPath) !== 0) {
-        return next(new BadRequest);
-      }
-
-      fs.stat(absolutePath, onStat.bind(null, absolutePath, i));
-    }
-
-    function onStat(absolutePath, i, err, stats) {
-      if (err || !stats.isFile()) { return next(new BadRequest); }
-
-      var mtime = new Date(stats.mtime);
-
-      if (!lastModified || mtime > lastModified) {
-        lastModified = mtime;
-      }
-
-      fs.readFile(absolutePath, 'utf8', onReadFile.bind(null, i));
-    }
-
     if (!pending) {
       // No files requested.
       return next(new BadRequest);
@@ -88,9 +55,35 @@ exports.combine = function (config) {
         return;
       }
 
-      fs.realpath(path.normalize(path.join(rootPath, relativePath)),
-          onRealPath.bind(null, i));
-    });
+      fs.realpath(path.normalize(path.join(rootPath, relativePath)), function (err, absolutePath) {
+        // Bubble up an error if the file can't be found or if the request
+        // attempts to traverse above the root path.
+        if (err || !absolutePath || absolutePath.indexOf(rootPath) !== 0) {
+          return next(new BadRequest);
+        }
+
+        fs.stat(absolutePath, function (err, stats) {
+          if (err || !stats.isFile()) { return next(new BadRequest); }
+
+          var mtime = new Date(stats.mtime);
+
+          if (!lastModified || mtime > lastModified) {
+            lastModified = mtime;
+          }
+
+          fs.readFile(absolutePath, 'utf8', function (err, data) {
+            if (err) { return next(new BadRequest); }
+
+            body[i]  = data;
+            pending -= 1;
+
+            if (pending === 0) {
+              finish();
+            }
+          }); // fs.readFile
+        }); // fs.stat
+      }); // fs.realpath
+    }); // forEach
   };
 };
 
@@ -123,27 +116,3 @@ function parseQuery(url) {
 
   return parsed;
 }
-
-/**
-Like Function.bind, but appends the supplied arguments to the end of the
-arguments list when calling the bound function, instead of prepending them to
-the beginning.
-
-@method rbind
-@param {Function} fn Function to bind.
-@param {Object} [thisArg] Object to be used as the `this` object when calling
-    the bound function.
-@param {mixed} [args*] Arguments to append to the arguments provided to the
-    bound function when called.
-@return {Function} Bound function.
-**/
-function rbind() {
-    var args    = Array.prototype.slice.call(arguments),
-        fn      = args.shift(),
-        thisArg = args.shift();
-
-    return function () {
-        fn.apply(thisArg, Array.prototype.slice.call(arguments).concat(args));
-    };
-}
-
