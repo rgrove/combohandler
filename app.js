b32ddbb7f811983a833b6bf8e2e4ddcc145f05cc
Ryan Grove <ryan@wonko.com>
2012-11-03 13:38:54 -0700

    No more Cluster.

--- app.js
+++ app.js
@@ -1,35 +1,4 @@
 #!/usr/bin/env node
 
-/**
-Cluster initialization file for the site. See the Cluster docs at
-<http://learnboost.github.com/cluster/> for details about Cluster.
-**/
-
-var cluster = require('cluster'),
-    server  = require('./lib/server');
-
-cluster(server(require('./config')))
-    .set('title', 'combohandler')
-
-    .in('development')
-        .set('workers', 1)
-        .use(cluster.pidfiles())
-        .use(cluster.cli())
-        .use(cluster.logger('logs', 'debug'))
-        .use(cluster.reload([
-                'config.js',
-                'index.js',
-                'lib',
-            ], {
-                extensions: ['.js'],
-                interval  : 1000,
-                signal    : 'SIGQUIT'
-            }))
-        .use(cluster.debug())
-        .listen(8000)
-
-    .in('production')
-        .use(cluster.pidfiles())
-        .use(cluster.cli())
-        .use(cluster.logger('logs'))
-        .listen(8000);
+var server = require('./lib/server');
+server(require('./config')).listen(8000);
