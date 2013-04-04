bc2e421c473c6fb1feeacf6fac29b88d0892c85d
Daniel Stockman <daniels@zillow.com>
2013-04-04 14:32:58 -0700

    Use assert.ifError instead of comparing to null.

--- test/server.js
+++ test/server.js
@@ -30,7 +30,7 @@ describe('combohandler', function () {
 
     it('should combine JavaScript', function (done) {
         request(BASE_URL + '/js?a.js&b.js', function (err, res, body) {
-            assert.equal(err, null);
+            assert.ifError(err);
             res.should.have.status(200);
             res.should.have.header('content-type', 'application/javascript;charset=utf-8');
             res.should.have.header('last-modified');
@@ -41,7 +41,7 @@ describe('combohandler', function () {
 
     it('should combine CSS', function (done) {
         request(BASE_URL + '/css?a.css&b.css', function (err, res, body) {
-            assert.equal(err, null);
+            assert.ifError(err);
             res.should.have.status(200);
             res.should.have.header('content-type', 'text/css;charset=utf-8');
             res.should.have.header('last-modified');
@@ -52,7 +52,7 @@ describe('combohandler', function () {
 
     it('should support symlinks pointing outside the root', function (done) {
         request(BASE_URL + '/js?a.js&b.js&outside.js', function (err, res, body) {
-            assert.equal(err, null);
+            assert.ifError(err);
             res.should.have.status(200);
             res.should.have.header('content-type', 'application/javascript;charset=utf-8');
             res.should.have.header('last-modified');
@@ -81,7 +81,7 @@ describe('combohandler', function () {
 
         it('should default to one year', function (done) {
             request(BASE_URL + '/js?a.js', function (err, res, body) {
-                assert.equal(err, null);
+                assert.ifError(err);
                 res.should.have.status(200);
                 res.should.have.header('cache-control', 'public,max-age=31536000');
 
@@ -94,7 +94,7 @@ describe('combohandler', function () {
 
         it('should not set Cache-Control and Expires headers when maxAge is null', function (done) {
             request(BASE_URL + '/max-age-null?a.js', function (err, res, body) {
-                assert.equal(err, null);
+                assert.ifError(err);
                 res.should.have.status(200);
                 res.headers.should.not.have.property('cache-control');
                 res.headers.should.not.have.property('expires');
@@ -104,7 +104,7 @@ describe('combohandler', function () {
 
         it('should support a maxAge of 0', function (done) {
             request(BASE_URL + '/max-age-0?a.js', function (err, res, body) {
-                assert.equal(err, null);
+                assert.ifError(err);
                 res.should.have.status(200);
                 res.should.have.header('cache-control', 'public,max-age=0');
 
@@ -120,7 +120,7 @@ describe('combohandler', function () {
     describe('errors', function () {
         it('should return a 400 Bad Request error when no files are specified', function (done) {
             request(BASE_URL + '/js', function (err, res, body) {
-                assert.equal(err, null);
+                assert.ifError(err);
                 res.should.have.status(400);
                 body.should.equal('Bad request. No files requested.');
                 done();
@@ -129,7 +129,7 @@ describe('combohandler', function () {
 
         it('should throw a 400 Bad Request error when a file is not found', function (done) {
             request(BASE_URL + '/js?bogus.js', function (err, res, body) {
-                assert.equal(err, null);
+                assert.ifError(err);
                 res.should.have.status(400);
                 res.should.have.header('content-type', 'text/plain; charset=utf-8');
                 body.should.equal('Bad request. File not found: bogus.js');
@@ -139,7 +139,7 @@ describe('combohandler', function () {
 
         it('should throw a 400 Bad Request error when a white-listed MIME type is not found', function (done) {
             request(BASE_URL + '/js?foo.bar', function (err, res, body) {
-                assert.equal(err, null);
+                assert.ifError(err);
                 res.should.have.status(400);
                 body.should.equal('Bad request. Illegal MIME type present.');
                 done();
@@ -148,7 +148,7 @@ describe('combohandler', function () {
 
         it('should throw a 400 Bad Request error when an unmapped MIME type is found with other valid types', function (done) {
             request(BASE_URL + '/js?a.js&foo.bar', function (err, res, body) {
-                assert.equal(err, null);
+                assert.ifError(err);
                 res.should.have.status(400);
                 body.should.equal('Bad request. Only one MIME type allowed per request.');
                 done();
@@ -157,7 +157,7 @@ describe('combohandler', function () {
 
         it('should throw a 400 Bad Request error when more than one valid MIME type is found', function (done) {
             request(BASE_URL + '/js?a.js&b.css', function (err, res, body) {
-                assert.equal(err, null);
+                assert.ifError(err);
                 res.should.have.status(400);
                 body.should.equal('Bad request. Only one MIME type allowed per request.');
                 done();
@@ -166,7 +166,7 @@ describe('combohandler', function () {
 
         it('should throw a 400 Bad Request error when a querystring is truncated', function (done) {
             request(BASE_URL + '/js?a.js&b', function (err, res, body) {
-                assert.equal(err, null);
+                assert.ifError(err);
                 res.should.have.status(400);
                 body.should.equal('Bad request. Truncated query parameters.');
                 done();
@@ -175,7 +175,7 @@ describe('combohandler', function () {
 
         it('should throw a 400 Bad Request error when a querystring is dramatically truncated', function (done) {
             request(BASE_URL + '/js?a', function (err, res, body) {
-                assert.equal(err, null);
+                assert.ifError(err);
                 res.should.have.status(400);
                 body.should.equal('Bad request. Truncated query parameters.');
                 done();
@@ -201,7 +201,7 @@ describe('combohandler', function () {
             paths.forEach(function (path) {
                 it('should throw a 400 Bad Request error when path traversal is attempted: ' + path, function (done) {
                     request(BASE_URL + '/js?' + path, function (err, res, body) {
-                        assert.equal(err, null);
+                        assert.ifError(err);
                         res.should.have.status(400);
                         res.should.have.header('content-type', 'text/plain; charset=utf-8');
                         body.should.match(/^Bad request. File not found: /);
@@ -238,7 +238,7 @@ describe('combohandler', function () {
 
         it ("should allow the basePath to end in a slash", function (done) {
             request(BASE_URL + "/rewrite-noslash?urls.css", function (err, res, body) {
-                assert.equal(err, null);
+                assert.ifError(err);
                 body.should.equal([
                     "#no-quotes { background: url(/rewritten/no-quotes.png);}",
                     "#single-quotes { background: url(\'/rewritten/single-quotes.png\');}",
@@ -255,7 +255,7 @@ describe('combohandler', function () {
         });
         it ("should not rewrite without a basePath", function (done) {
             request(BASE_URL + "/norewrite?urls.css", function (err, res, body) {
-                assert.equal(err, null);
+                assert.ifError(err);
                 body.should.equal([
                     "#no-quotes { background: url(no-quotes.png);}",
                     "#single-quotes { background: url(\'single-quotes.png\');}",
@@ -272,7 +272,7 @@ describe('combohandler', function () {
         });
         it ("should rewrite valid urls", function (done) {
             request(BASE_URL + "/rewrite?urls.css&deeper/more.css", function (err, res, body) {
-                assert.equal(err, null);
+                assert.ifError(err);
                 body.should.equal([
                     "#no-quotes { background: url(/rewritten/no-quotes.png);}",
                     "#single-quotes { background: url(\'/rewritten/single-quotes.png\');}",
