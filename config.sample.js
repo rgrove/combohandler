c4b949bfd17b059c4ba80b84b313410a7fe6227c
Daniel Stockman <daniels@zillow.com>
2013-04-04 17:43:38 -0700

    Add dynamicPath middleware.
    This supports route params (e.g., :version) that point to different trees under the same filesystem root.

--- config.sample.js
+++ config.sample.js
@@ -7,7 +7,17 @@ module.exports = {
     //
     //   http://example.com/yui3?build/yui/yui-min.js&build/loader/loader-min.js
     //
+    // The :version placeholder is used to version the combo-handled requests by
+    // "global" directory name (usually a sha1 hash), instead of per-file.
+    //
+    // The URL and local path must both contain the placeholder ":version"
+    //
+    //   http://example.com/84b94bb/combo?build/mod-a/mod-a-min.js&build/mod-b/mod-b-min.js
+    //     resolves to:
+    //       /local/path/to/84b94bb/build/mod-a/mod-a-min.js
+    //       /local/path/to/84b94bb/build/mod-b/mod-b-min.js
     roots: {
+        '/:version/combo': '/local/path/to/:version',
         '/yui3': '/local/path/to/yui3'
     },
 
