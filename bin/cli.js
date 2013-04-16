532d626bf379959b4f717ee98492d1be844cb193
Daniel Stockman <daniels@zillow.com>
2013-04-15 17:30:20 -0700

    Add --cluster option to CLI, defaulting to single-threaded default server.

--- bin/cli.js
+++ bin/cli.js
@@ -1,14 +1,28 @@
 #!/usr/bin/env node
 
-var args = require('../lib/args'),
-    comboCluster = require('../lib/cluster');
+var options = require('../lib/args').parse();
 
-comboCluster(args.parse());
+if (options.cluster) {
+    // Cluster constructor calls #listen() if no other action
+    require('../lib/cluster')(options);
+} else {
+    // Legacy support
+    var instance = require('../lib/server')(options);
+    var port = options.port || require('../lib/defaults').master.port;
+
+    if (!options.quiet) {
+        console.log("combohandler: Listening on http://localhost:%d/", port);
+    }
+
+    instance.listen(port);
+}
+
 
 //  Local "integration" test from root of combohandler repo
 //
 //  Terminal:
 //      ./bin/cli.js -f ./test/root.json
+//      ./bin/cli.js --cluster -f ./test/root.json
 //
 //  Browser:
 //      localhost:8000/test?js/a.js&js/b.js
