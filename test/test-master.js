4ea5a49b3641b8df3f19c7110a596b361626ce8f
Daniel Stockman <daniels@zillow.com>
2013-04-16 19:39:47 -0700

    ComboMaster extends ComboBase.

--- test/test-master.js
+++ test/test-master.js
@@ -1,18 +1,44 @@
 /*global describe, before, after, it */
 
+var ComboBase = require('../lib/cluster/base');
 var ComboMaster = require('../lib/cluster');
 
 describe("cluster master", function () {
     describe("instantiation", function () {
-        it("should support factory-style (no new)");
-        it("should merge defaults with passed options");
+        it("should support empty options arg with correct defaults", function () {
+            var instance = new ComboMaster();
+
+            instance.should.have.property('options');
+            instance.options.should.eql(ComboMaster.defaults);
+        });
+
+        it("should support factory-style (no new)", function () {
+            /*jshint newcap: false */
+            var instance = ComboMaster();
+
+            instance.should.be.an.instanceOf(ComboMaster);
+            instance.should.have.property('options');
+        });
+
+        it("should be an instance of ComboBase", function () {
+            var instance = new ComboMaster();
+
+            instance.should.be.an.instanceOf(ComboBase);
+        });
     });
 
-    describe("#start()", function () {
-        it("should start");
+    describe("on 'start'", function () {
+        it("should setupMaster");
+        it("should create master.pid");
+        it("should attach events");
     });
 
-    describe("#listen()", function () {
-        it("should listen");
+    describe("on 'destroy'", function () {
+        it("should detach events, passing through callback");
+        it("should not error when detachEvents callback missing");
+    });
+
+    describe("on 'listen'", function () {
+        it("should fork workers");
     });
 });
