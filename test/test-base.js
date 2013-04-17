38a989c76c7db06b39d03b89c80036776aed4ab0
Daniel Stockman <daniels@zillow.com>
2013-04-16 19:23:38 -0700

    Extract base class from cluster master and worker.

---
+++ test/test-base.js
@@ -0,0 +1,86 @@
+/*global describe, it */
+var path = require('path');
+
+var ComboBase = require('../lib/cluster/base');
+
+describe("cluster base", function () {
+
+    describe("instantiation", function () {
+        it("should support empty options arg", function () {
+            var instance = new ComboBase();
+
+            instance.should.have.property('options');
+            instance.options.should.eql({});
+        });
+
+        it("should merge passed options into instance.options", function () {
+            var instance = new ComboBase({
+                port: 8080
+            });
+
+            instance.options.port.should.equal(8080);
+        });
+
+        it("should clean merged options", function () {
+            var instance = new ComboBase({
+                server: 'test/fixtures/server.js'
+            });
+
+            instance.options.server.should.equal(path.resolve('test/fixtures/server.js'));
+        });
+    });
+
+    describe("#start()", function () {
+        it("should be chainable (return this)", function () {
+            var instance = new ComboBase();
+
+            instance.should.equal(instance.start());
+        });
+
+        it("should call provided callback", function (done) {
+            var instance = new ComboBase();
+
+            instance.start(done);
+        });
+    });
+
+    describe("#destroy()", function () {
+        it("should set _destroyed property", function () {
+            var instance = new ComboBase();
+
+            instance.destroy();
+
+            instance.should.have.property('_destroyed');
+            instance._destroyed.should.equal(true);
+        });
+
+        it("should call destroy callback directly when no listener for 'destroy' event", function (done) {
+            var instance = new ComboBase();
+
+            instance.destroy(done);
+        });
+    });
+
+    describe("#listen()", function () {
+        it("should modify instance options with optional port arg", function () {
+            var instance = new ComboBase();
+
+            // override start() to prevent execution
+            instance.start = function () {};
+
+            instance.listen(1);
+            instance.options.port.should.equal(1);
+        });
+
+        it("should call #start() with callback", function (done) {
+            var instance = new ComboBase();
+
+            // override _listen() to signal completion
+            // instance._listen = done;
+            instance.on('listen', done);
+
+            instance.listen();
+        });
+    });
+
+});
